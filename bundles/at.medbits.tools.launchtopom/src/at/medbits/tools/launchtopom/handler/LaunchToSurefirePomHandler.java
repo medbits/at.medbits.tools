24efd85ec46ec38b1b5f1f9955c5b49fbdfcf3ee
Marco Descher <marco@descher.at>
2021-09-23 12:47:53 +0200

    Determine artifactId, consider parent of parent for version, add missing version for target-platform-configuration

--- bundles/at.medbits.tools.launchtopom/src/at/medbits/tools/launchtopom/handler/LaunchToSurefirePomHandler.java
+++ bundles/at.medbits.tools.launchtopom/src/at/medbits/tools/launchtopom/handler/LaunchToSurefirePomHandler.java
@@ -27,6 +27,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -37,8 +38,14 @@ import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.pde.internal.core.bundle.WorkspaceBundlePluginModel;
+import org.eclipse.pde.internal.core.ibundle.IBundle;
+import org.eclipse.pde.internal.core.ibundle.IBundleModel;
+import org.eclipse.pde.internal.core.ibundle.IBundlePluginModelBase;
+import org.eclipse.pde.internal.core.project.PDEProject;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.osgi.framework.Constants;
 
 import at.medbits.tools.launchtopom.handler.model.FragmentRequirementsXml;
 import at.medbits.tools.launchtopom.handler.model.LaunchBundles;
@@ -95,21 +102,35 @@ public class LaunchToSurefirePomHandler extends AbstractHandler implements IHand
 			LaunchBundles workspaceBundles = LaunchBundles
 					.of((Collection<String>) attributes.get("selected_workspace_bundles"), Type.WORKSPACE);
 
-			String artifactId = file.getProject().getName();
+			String artifactId = determineArtifactId(file);
 			org.apache.maven.model.Model model = new Model();
 			model.setArtifactId(artifactId);
 			model.setPackaging("eclipse-test-plugin");
 			model.setModelVersion("4.0.0");
 
-			File parentPom = lookupParentPom(file);
+			File parentPom = lookupParentPom(file.getRawLocation());
 			if (parentPom != null) {
 				MavenXpp3Reader reader = new MavenXpp3Reader();
+				String parentArtifactId = null;
+				String version = null;
+				String groupId = null;
 				try (FileInputStream fin = new FileInputStream(parentPom)) {
 					Model parentContent = reader.read(fin);
+					parentArtifactId = parentContent.getArtifactId();
+					version = parentContent.getVersion();
+					groupId = parentContent.getGroupId();
+					
+					if(version == null) {
+						version = parentContent.getParent().getVersion();
+					}
+					if(groupId == null) {
+						groupId = parentContent.getParent().getGroupId();
+					}
+					
 					Parent parent = new Parent();
-					parent.setArtifactId(parentContent.getArtifactId());
-					parent.setVersion(parentContent.getVersion());
-					parent.setGroupId(parentContent.getGroupId());
+					parent.setArtifactId(parentArtifactId);
+					parent.setVersion(version);
+					parent.setGroupId(groupId);
 					model.setParent(parent);
 				}
 			} else {
@@ -147,9 +168,37 @@ public class LaunchToSurefirePomHandler extends AbstractHandler implements IHand
 		return false;
 	}
 
-	private File lookupParentPom(IFile file) {
+	/**
+	 * If its a PDE project, the artifact id has to be the same as the bundle
+	 * symbolic name, so try to resolve this. If it fails, return the folder name.
+	 * 
+	 * @param file
+	 * @return
+	 */
+	@SuppressWarnings("restriction")
+	private String determineArtifactId(IFile file) {
+		IProject project = file.getProject();
+		
+		IFile pluginXml = PDEProject.getPluginXml(project);
+		IFile manifest = PDEProject.getManifest(project);
+		
+		if (pluginXml != null && manifest != null) {
+			WorkspaceBundlePluginModel model = new WorkspaceBundlePluginModel(manifest, pluginXml);
+			if (model != null && (model instanceof IBundlePluginModelBase)) {
+				IBundlePluginModelBase bundlePModel = (IBundlePluginModelBase) model;
+				IBundleModel bundleModel = bundlePModel.getBundleModel();
+				IBundle bundle = bundleModel.getBundle();
+				String bundleSymbolicName = bundle.getHeader(Constants.BUNDLE_SYMBOLICNAME);
+				if (bundleSymbolicName != null && bundleSymbolicName.length() > 0) {
+					return bundleSymbolicName;
+				}
+			}
+		}
+		return project.getName();
+	}
+
+	private File lookupParentPom(IPath fileLocation) {
 		// parent should be project
-		IPath fileLocation = file.getRawLocation();
 		IPath parentDir = fileLocation.removeLastSegments(2);
 		File parentPom = parentDir.append("pom.xml").toFile();
 		if (parentPom != null && parentPom.exists()) {
@@ -179,7 +228,7 @@ public class LaunchToSurefirePomHandler extends AbstractHandler implements IHand
 		Plugin plugin = new Plugin();
 		plugin.setGroupId("org.eclipse.tycho");
 		plugin.setArtifactId("target-platform-configuration");
-
+		plugin.setVersion("${tycho-version}");
 		workspaceBundles = LaunchBundles.fragments(workspaceBundles);
 		targetBundles = LaunchBundles.fragments(targetBundles);
 		try {
